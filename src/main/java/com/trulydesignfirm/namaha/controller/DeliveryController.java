25df12df5bae3db0c70075901b6132dbbb36b926
dhruv <dhruvgupta130@gmail.com>
2025-09-23 02:52:49 +0530

    Updated Delivery Side

--- src/main/java/com/trulydesignfirm/namaha/controller/DeliveryController.java
+++ src/main/java/com/trulydesignfirm/namaha/controller/DeliveryController.java
@@ -3,6 +3,7 @@ package com.trulydesignfirm.namaha.controller;
 import com.trulydesignfirm.namaha.constant.DeliveryStatus;
 import com.trulydesignfirm.namaha.dto.Response;
 import com.trulydesignfirm.namaha.service.DeliveryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +20,17 @@ public class DeliveryController {
     private final DeliveryService deliveryService;
 
     @GetMapping("/pending")
-    public ResponseEntity<Response> pending(@RequestParam int pageNumber, @RequestParam int pageSize) {
+    @Operation(summary = "Get all pending deliveries")
+    public ResponseEntity<Response> pending(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "10") int pageSize
+    ) {
         Response allDeliveries = deliveryService.getAllPendingDeliveries(pageNumber, pageSize);
         return new ResponseEntity<>(allDeliveries, allDeliveries.status());
     }
 
     @PutMapping("/update/{id}")
+    @Operation(summary = "Update delivery status")
     public ResponseEntity<Response> update(@PathVariable UUID id, @RequestParam DeliveryStatus status) {
         Response response = deliveryService.updateDeliveryStatus(id, status);
         return new ResponseEntity<>(response, response.status());
--- src/main/java/com/trulydesignfirm/namaha/scheduled/DeliveryScheduler.java
+++ src/main/java/com/trulydesignfirm/namaha/scheduled/DeliveryScheduler.java
@@ -5,12 +5,15 @@ import com.trulydesignfirm.namaha.model.Delivery;
 import com.trulydesignfirm.namaha.repository.DeliveryRepo;
 import com.trulydesignfirm.namaha.repository.SubscriptionRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DeliveryScheduler {
@@ -18,14 +21,17 @@ public class DeliveryScheduler {
     private final SubscriptionRepo subscriptionRepo;
     private final DeliveryRepo deliveryRepo;
 
-    @Scheduled(cron = "0 0 2 * * ?")
+//    @Scheduled(cron = "0 0 2 * * ?")
+    @Scheduled(fixedRate = 86400000)
     @Transactional
     public void createDailyDeliveries() {
+        log.info("Creating daily deliveries on {}", LocalDate.now());
         List<Delivery> list = subscriptionRepo
                 .findAllByStatus(SubscriptionStatus.ACTIVE)
                 .stream()
                 .map(Delivery::new)
                 .toList();
+        log.info("Creating {} deliveries from subscriptions on {}", list.size(), LocalDate.now());
         deliveryRepo.saveAll(list);
     }
 }
